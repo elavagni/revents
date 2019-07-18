204ed902dc829ac58b8fa65d9cfa5b751c9051b4
eric <elavagnim@gmail.com>
2019-07-18 17:55:56 -0500

    Added testarea folder, to play with new redux concepts (store, actions, reducers)

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -9,6 +9,7 @@ import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
 import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
 import EventForm from "../../features/event/EventForm/EventForm";
 import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import TestComponent from "../../features/testarea/TestComponent";
 
 class App extends Component {
   render() {
@@ -27,6 +28,7 @@ class App extends Component {
                 <Route path="/profile/:id" component={UserDetailedPage} />
                 <Route path="/settings/" component={SettingsDashboard} />
                 <Route path="/createEvent/" component={EventForm} />
+                <Route path="/test/" component={TestComponent} />
               </Container>
             </Fragment>
           )}
